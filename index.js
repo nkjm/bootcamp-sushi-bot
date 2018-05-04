1282caeb4411e686ec4db7aa101a4e0a6db69ea2
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-05-04 16:14:06 +0900

    Remove unnecessary options

--- index.js
+++ index.js
@@ -32,10 +32,6 @@ server.use("/webhook", bot_express({
     },
     line_channel_secret: process.env.LINE_CHANNEL_SECRET,
     line_access_token: process.env.LINE_ACCESS_TOKEN,
-    facebook_app_secret: process.env.FACEBOOK_APP_SECRET,
-    facebook_page_access_token: [
-        {page_id: process.env.FACEBOOK_PAGE_ID, page_access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN}
-    ],
     default_skill: process.env.DEFAULT_SKILL,
     google_project_id: process.env.GOOGLE_PROJECT_ID,
     auto_translation: process.env.AUTO_TRANSLATION
